72c602ee702123b9b1a9250830089d85753fab21
petscheit <etscheitp@gmail.com>
2024-12-20 16:47:04 +0100

    chore: lib fix

--- evm_proof_faker/src/lib.rs
+++ evm_proof_faker/src/lib.rs
@@ -4,10 +4,10 @@ use eth_trie_proofs::storage::StorageMptHandler;
 use ethereum_types::H256;
 use url::Url;
 
-struct Instance {
+pub struct Instance {
     handler: StorageMptHandler,
 }
-trait ProofFaker<T> {
+pub trait ProofFaker {
     fn new(url: String) -> Self;
     async fn fake_storage_slot(
         &self,
@@ -18,7 +18,7 @@ trait ProofFaker<T> {
     ) -> Result<String, EthTrieError>;
 }
 
-impl ProofFaker<StorageMptHandler> for Instance {
+impl ProofFaker for Instance {
     fn new(url: String) -> Self {
         let url = Url::parse(&url).unwrap();
         Self {
@@ -48,22 +48,4 @@ impl ProofFaker<StorageMptHandler> for Instance {
             .unwrap();
         Ok(root)
     }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), EthTrieError> {
-    let url: String = "https://mainnet.infura.io/v3/66dda5ed7d56432a82c8da4ac54fde8e".to_string();
-    let address: String = "Ca14007Eff0dB1f8135f4C25B34De49AB0d42766".to_string();
-    let storage_slot: String =
-        "4d13244817f246930fdc27dd358d16eb57bb7af945c5c4daddbee79636769dc8".to_string();
-    let block_number: String = "1466AB0".to_string();
-    let new_value: String = "00".to_string();
-    let instance = Instance::new(url.to_string());
-    let root = instance
-        .fake_storage_slot(address, storage_slot, block_number, new_value)
-        .await?;
-
-    println!("root: {:?}", root);
-
-    Ok(())
 }
